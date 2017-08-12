e788cd5115ee232acae52b9fa233a1e2ca13fa15
Chris Wiesen <mpyr6@yahoo.com>
2017-08-11 18:21:47 -0700

    Blocked out Argv input method

--- app.js
+++ app.js
@@ -6,31 +6,32 @@ const express = require('express');
 const mongoose = require('mongoose');
 var fillUpData = require('./models/mpgs');
 
-const argv = yargs
-  .options({
-    miles: {
-      demand: true,
-      describe: 'How many miles since last fill-up',
-      type: 'number'
-    },
-    gallons: {
-      demand: true,
-      describe: 'How many gallons to fill tank',
-      type: 'number'
-    },
-    price: {
-      demand: true,
-      describe: 'Price of gas',
-      type: 'number'
-    },
-    gas: {
-      demand: true,
-      describe: 'Gas Type',
-      choices: [87,89,91]
-    }
-  })
-  .help()
-  .argv;
+// Input though Terminal
+// const argv = yargs
+//   .options({
+//     miles: {
+//       demand: true,
+//       describe: 'How many miles since last fill-up',
+//       type: 'number'
+//     },
+//     gallons: {
+//       demand: true,
+//       describe: 'How many gallons to fill tank',
+//       type: 'number'
+//     },
+//     price: {
+//       demand: true,
+//       describe: 'Price of gas',
+//       type: 'number'
+//     },
+//     gas: {
+//       demand: true,
+//       describe: 'Gas Type',
+//       choices: [87,89,91]
+//     }
+//   })
+//   .help()
+//   .argv;
 
 var miles = argv.miles;
 var gallons = argv.gallons;
@@ -55,7 +56,7 @@ var entry = {
   efficiency: mpg(miles,gallons),
   gasPrice: price,
   fuel: gas
-}
+};
 
 fillUpData.create(entry, function(err, datas){
   if(err){
